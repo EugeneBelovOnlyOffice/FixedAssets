ade7c77a94aca27a5f07ae48666a46f55160e18c
Eugene <banbelov@gmail.com>
2022-02-21 16:54:07 +0300

    pulldata

--- src/components/List.js
+++ src/components/List.js
@@ -3,32 +3,61 @@ import AddIcon from "@mui/icons-material/Add";
 import { IconButton, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import DataTable from "./DataTable";
-import { collection, query, where, getDocs,getDoc,doc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
 import { Context } from "../index";
+import SelectItem from "./SelectItem";
 
 const List = () => {
+
+  
+//Liffted state from SelectItem
+
+const [selectDevice, setDevice] = React.useState('All');
+
+const handleChangeDevice = (selectDevice) => {
+  setDevice(selectDevice);
+};
+
+///
+ 
   const { firestore } = useContext(Context);
-  const q = query(collection(firestore, "Laptop"), where('owner','==','Eugene Belov'));
+  const q = query(
+    collection(firestore, selectDevice)
+  );
 
+  const pullCollection = async () => {
+    const querySnapshot = await getDocs(q);
 
-  const pullCollection = async () =>{
-  const querySnapshot = await  getDocs(q);
-
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-});}
-
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+    console.log(doc.data());
+ 
+    });
+  
+  };
 
   return (
     <>
       <IconButton component={Link} to="/add" color="inherit">
         <AddIcon fontSize="large" color="primary" />
       </IconButton>
-      <Button onClick={()=>pullCollection()} variant="outlined" color="primary">
-            Pull
-          </Button>
-   
+      <SelectItem handleChangeDevice = {handleChangeDevice.bind(this)} selectDevice={{selectDevice}}/>
+    
+      <Button
+        onClick={() => pullCollection()}
+        variant="outlined"
+        color="primary"
+      >
+        Pull
+      </Button>
     </>
   );
 };
