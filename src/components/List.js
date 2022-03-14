6c4285648f38ff3567bca31ac521100d27dfc469
Eugene <banbelov@gmail.com>
2022-03-14 15:58:09 +0300

    search

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
-import { IconButton, Button } from "@mui/material";
+import { IconButton, Button, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
 import DataTable from "./DataTable";
 import {
@@ -15,34 +15,51 @@ import {
 import { Context } from "../index";
 import SelectItem from "./SelectItem";
 import ItemCard from "../FACards/ItemCard";
+import { async } from "@firebase/util";
 
 const List = () => {
   //Liffted state from SelectItem
 
   const [selectDevice, setDevice] = React.useState("Desktop");
   const [firebaseArray, setfirebaseArray] = useState([]);
+  const [param, setParam] = useState('');
 
   const handleChangeDevice = (selectDevice) => {
     setDevice(selectDevice);
   };
 
-
-
-
-
   const { firestore } = useContext(Context);
-  const q = query(collection(firestore, selectDevice));
+  const col = collection(firestore, selectDevice);
+ // const q = query(col, where("fa", "==", param));
 
   const pullCollection = async () => {
+    const q = query(col);
     const querySnapshot = await getDocs(q);
     const items = [];
     querySnapshot.forEach((doc) => {
       items.push(doc.data());
     });
     setfirebaseArray(items);
-    
+
     console.log(firebaseArray);
+ 
   };
+
+  const pullOnChange = async (event) => {   
+setParam(event)
+console.log(event)
+const q = query(col, where("fa", "==", event));
+const querySnapshot = await getDocs(q);
+const items = [];
+querySnapshot.forEach((doc) => {
+  items.push(doc.data());
+});
+setfirebaseArray(items);
+
+console.log(firebaseArray);
+ 
+  }
+
 
   return (
     <>
@@ -53,7 +70,7 @@ const List = () => {
         handleChangeDevice={handleChangeDevice.bind(this)}
         selectDevice={{ selectDevice }}
       />
-
+      <TextField onChange={(e)=>pullOnChange(e.target.value)} fullWidth label="Search"></TextField>
       <Button
         onClick={() => pullCollection()}
         variant="outlined"
@@ -62,7 +79,9 @@ const List = () => {
         Pull
       </Button>
       {firebaseArray.map((item) => (
-        <ItemCard key={item.sn} sn={item.sn}
+        <ItemCard
+          key={item.sn}
+          sn={item.sn}
           dept={item.dept}
           fa={item.fa}
           model={item.model}
